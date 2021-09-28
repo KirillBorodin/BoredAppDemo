dcaa14af30d5a2116db4022f9ff6a4762312f1f7
Kirill <kborodin.dev@gmail.com>
2021-09-28 20:05:40 +0300

    initial

---
+++ android/app/src/main/java/com/epam/kborodin/bored/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.epam.kborodin.bored
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
